81cbc36837e4e7913b6fec51741980536a6b6789
cameronjoebrown <cameronjoebrown@gmail.com>
2023-05-07 16:44:39 -0600

    Add send_request method

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-class Scraper():
-    # website = requests.get(self.get_url(), timeout=5)
+class Scraper:
+    
 
     # website.raise_for_status()
 
@@ -10,5 +10,8 @@ class Scraper():
 
     # week22 = website_content.select('table:-soup-contains("Week 22")')
 
-    def get_url():
+    def get_url(self):
         return "https://www.ironblosam.net/for_sale_by_owner.php"
+
+    def send_request(self):
+        return requests.get(self.get_url(), timeout=5)
--- test_scraper.py
+++ test_scraper.py
@@ -1,11 +1,21 @@
 import unittest
+from unittest.mock import Mock, patch
 from scraper import Scraper
 
 class TestScraper(unittest.TestCase):
+    def setUp(self):
+        self.scraper = Scraper()
+
     def test_get_url(self):
         url = 'https://www.ironblosam.net/for_sale_by_owner.php'
-        result = Scraper.get_url()
-        self.assertEqual(result, url);
+        result = self.scraper.get_url()
+        self.assertEqual(result, url)
+
+    def test_send_request_should_return_response(self):
+        mock_response = Mock(status_code=200, json=Mock(return_value={"foo": "bar"}))
+        with patch("requests.get", return_value=mock_response):
+            result = self.scraper.send_request()
+            self.assertEqual(result, mock_response)
         
 if __name__ == '__main__':
     unittest.main()
